e9e63afb9bc6103f8190867f67611a30178ec408
nsikak essien <nsikakessien66@gmail.com>
2022-02-14 15:48:37 +0100

    video search

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Youtube from "../apis/Youtube";
+import SearchBar from "./SearchBar";
+import VideoDetail from "./VideoDetail";
+import VideoList from "./VideoList";
+
+export class App extends Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("music");
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await Youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
